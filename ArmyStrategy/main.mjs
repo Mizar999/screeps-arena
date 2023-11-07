4ae835b34846193684c5a8401f2801d70b2f9a42
Mario <mizar2502@aol.com>
2023-11-07 13:13:42 +0100

    Fix call order

--- ArmyStrategy/main.mjs
+++ ArmyStrategy/main.mjs
@@ -5,13 +5,13 @@ import { StrategyManager } from "./src/strategies/strategy-manager";
 let strategy;
 export function loop() {
     GameManager.updateCache();
-
     if (!strategy) {
         strategy = StrategyManager.getStrategy();
     }
-    try { strategy.createArmy(); } catch (e) { console.log(e); }
 
     try { ArmyManager.cleanup(); } catch (e) { console.log(e); }
+
+    try { strategy.createArmy(); } catch (e) { console.log(e); }
     try { ArmyManager.applyStrategies(); } catch (e) { console.log(e); }
     try { ArmyManager.spawn(); } catch (e) { console.log(e); }
 }

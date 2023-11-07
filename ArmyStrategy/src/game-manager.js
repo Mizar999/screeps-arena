ddb49e270cbeb3c3090a09e867e0802e1f9cdbfe
Mario <mizar2502@aol.com>
2023-11-07 01:49:47 +0100

    Update game manager

--- ArmyStrategy/src/game-manager.js
+++ ArmyStrategy/src/game-manager.js
@@ -3,23 +3,28 @@ import * as prototypes from "game/prototypes";
 
 export class GameManager {
     static myCreeps;
+    static myDamagedCreeps;
     static mySpawn;
     static enemies;
     static enemySpawn;
 
     static updateCache() {
         this.myCreeps = [];
+        this.myDamagedCreeps = [];
         this.enemies = [];
         utils.getObjectsByPrototype(prototypes.Creep).forEach(creep => {
             if (creep.my) {
                 this.myCreeps.push(creep);
+                if (creep.hits < creep.hitsMax) {
+                    this.myDamagedCreeps.push(creep);
+                }
             } else {
                 this.enemies.push(creep);
             }
         });
 
         utils.getObjectsByPrototype(prototypes.StructureSpawn).forEach(spawn => {
-            if(spawn.my) {
+            if (spawn.my) {
                 this.mySpawn = spawn;
             } else {
                 this.enemySpawn = spawn;
